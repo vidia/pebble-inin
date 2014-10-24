bca0fed104ebec581ba65ef158d7a2eae05d2b07
David Tschida <dmtschida1@gmail.com>
2014-10-24 15:13:28 -0400

    Font

--- src/inin.c
+++ src/inin.c
@@ -4,6 +4,8 @@
 // App-specific data
 Window *window; // All apps must have at least one window
 TextLayer *time_layer; // The clock
+TextLayer *name_layer;
+
 
 static GBitmap *foreground_image;
 static GBitmap *background_image;
@@ -38,14 +40,25 @@ static void do_init(void) {
 
   Layer *window_layer = window_get_root_layer(window);
 
-  font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MAIN_60)); 
+  font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MAIN_18)); 
 
   // Init the text layer used to show the time
-  time_layer = text_layer_create(GRect(0, 85, 144 /* width */, 168-85 /* height */));
+  time_layer = text_layer_create(GRect(0, 120, 144 /* width */, 168-120 /* height */));
   text_layer_set_text_color(time_layer, GColorWhite);
   text_layer_set_background_color(time_layer, GColorClear);
   text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
-  text_layer_set_font(time_layer, font);
+  text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+
+
+  // Init the text layer used to show the company name
+  name_layer = text_layer_create(GRect(0, 90, 144 /* width */, 168-85 /* height */));
+  text_layer_set_text_color(name_layer, GColorWhite);
+  text_layer_set_background_color(name_layer, GColorClear);
+  text_layer_set_text_alignment(name_layer, GTextAlignmentCenter);
+  text_layer_set_font(name_layer, font);
+  text_layer_set_text(name_layer, "Interactive Intelligence");
+
+
 
   foreground_image = gbitmap_create_with_resource(RESOURCE_ID_LOGO_FORE);
   background_image = gbitmap_create_with_resource(RESOURCE_ID_LOGO_BACK);
@@ -74,10 +87,12 @@ static void do_init(void) {
   tick_timer_service_subscribe(SECOND_UNIT, &handle_second_tick);
 
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(name_layer));
 }
 
 static void do_deinit(void) {
   text_layer_destroy(time_layer);
+  text_layer_destroy(name_layer);
   window_destroy(window);
   bitmap_layer_destroy(foreground_image_layer);
   bitmap_layer_destroy(background_image_layer);
